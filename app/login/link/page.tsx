0394c6a131df2131a29c68c1f28f12e7ea25689e
EstevanKov <franciscoesteban15rdz@gmail.com>
2024-04-26 20:01:02 -0600

    test

--- app/login/link/page.tsx
+++ app/login/link/page.tsx
@@ -3,36 +3,34 @@ import { headers, cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-export default function Login({
+export default function MagicLink({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signIn = async (formData: FormData) => {
+  const SendEmail = async (formData: FormData) => {
     "use server";
 
     const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
+    const origin = headers().get("origin");
 
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signInWithOtp({
       email,
-      password,
+      options: {
+        shouldCreateUser: false,
+        emailRedirectTo: `${origin}/auth/callback`
+      }
     });
 
-    if (error !== null) {
-      console.error(new Date());
-      console.error("Ocurrió un error inesperado...");
-      console.error("FOLIO: xxx");
-      console.error("mensaje = \"BAD SESSION\";");
-      console.error("Error al autenticar usuario:", error.message);
-      return redirect("/login?message=Error al autenticar usuario");
+    if (error) {
+      return redirect("/login/link?message=No se pudo validar el correo");
     }
 
-    return redirect("/");
+    return redirect("/login/link?message=Te hemos enviado tu acceso al email");
   };
-
+/*
   const signUp = async (formData: FormData) => {
     "use server";
 
@@ -46,17 +44,16 @@ export default function Login({
       email,
       password,
       options: {
-        emailRedirectTo: `${origin}/auth/callback`,
+        emailRedirectTo: ${origin}/auth/callback,
       },
     });
 
-    if (error !== null) {
-      console.error("Error al registrar usuario:", error.message);
+    if (error) {
       return redirect("/login?message=Could not authenticate user");
     }
 
     return redirect("/login?message=Check email to continue sign in process");
-  };
+  };*/
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
@@ -83,8 +80,9 @@ export default function Login({
 
       <form
         className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
+        action={SendEmail}
       >
+        <p>Inicia sesion con el link que te enviaremos a tu correo</p>
         <label className="text-md" htmlFor="email">
           Email
         </label>
@@ -94,26 +92,9 @@ export default function Login({
           placeholder="you@example.com"
           required
         />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        
-       <center> <p>click aquí para recuperar tu sesión <u><a href="http://localhost:3000/login/link">Página de recuperación</a></u></p></center>
+
         <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign Up
+          Enviarme acceso rápido
         </button>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
@@ -121,6 +102,6 @@ export default function Login({
           </p>
         )}
       </form>
-    </div>
-  );
+    </div>
+  );
 }
